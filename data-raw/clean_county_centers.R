be4c07a5d35cc5157343d867a939633c98e37e21
Brooke Anderson <brooke.anderson@colostate.edu>
2016-08-17 10:37:54 -0600

    Try to fix tilde in county name for `county_centers`

--- data-raw/clean_county_centers.R
+++ data-raw/clean_county_centers.R
@@ -1,7 +1,11 @@
+library(dplyr)
+library(stringi)
+
 ## Pull in data from the US Census on county population centers, names, and FIPS codes
 census_csv <- paste0("http://www2.census.gov/geo/docs/reference/cenpop2010/",
                      "county/CenPop2010_Mean_CO.txt")
 census_data <- utils::read.csv(census_csv)
+census_data$COUNAME <- stri_trans_general(census_data$COUNAME, "latin-ascii")
 census_data$COUNTYFP <- sprintf("%03d", census_data$COUNTYFP)
 census_data <- dplyr::mutate(census_data, choro_fips = paste0(census_data$STATEFP,
                                                               census_data$COUNTYFP))
