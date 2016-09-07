52b42b86331ee964a5389bed2dc1d1759d84526d
Rachel Severson <rachel.severson@colostate.edu>
2016-09-07 15:08:30 -0600

    fix errors in county radius cleaning

--- data-raw/clean_county_radii.R
+++ data-raw/clean_county_radii.R
@@ -18,8 +18,9 @@ area$state_fips <- sprintf("%02d", area$state_fips)
 area$county_fips <- sprintf("%03d", area$county_fips)
 area$fips <- paste0(area$state_fips, area$county_fips)
 
-area <- dplyr::mutate(test, land_area_km = land_area / 1,000,000) %>%
-  dplyr::mutate(county_radius = sqrt(land_area_km / pi))
+area <- dplyr::mutate(area, land_area_km = (land_area / 1000000)) %>%
+  dplyr::mutate(county_radius = sqrt(land_area_km / pi)) %>%
+  dplyr::select_(.dots = c("fips", "county"))
 
 county_radius <- area
 devtools::use_data(county_radius, overwrite = TRUE)
