76790730338cdbfe08c72d3bea55781d6d6162a9
Rachel Severson <rachel.severson@colostate.edu>
2016-09-14 17:59:40 -0600

    update county_outlines cleaning

--- data-raw/clean_county_outlines.R
+++ data-raw/clean_county_outlines.R
@@ -1,11 +1,12 @@
 library(rgdal)
+library(dplyr)
+library(raster)
 
 # Download U.S. Census 2015 Cartographic Boundary Shapefiles - Counties from
 # https://www.census.gov/geo/maps-data/data/cbf/cbf_counties.html
 # download cb_2015_us_county_20m.zip
 
-# setwd("~/Desktop/cb_2015_us_county_20m")
-mapdata <- readOGR("cb_2015_us_county_20m.shp", layer = "cb_2015_us_county_20m")
+mapdata <- shapefile("~/Desktop/cb_2015_us_county_20m/cb_2015_us_county_20m.shp")
 
 mapdata@data <- mutate(mapdata@data, STATEFP = as.character(STATEFP))
 mapdata@data <- mutate(mapdata@data, COUNTYFP = as.character(COUNTYFP))
