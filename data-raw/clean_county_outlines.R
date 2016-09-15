1dd7517d1d753f793b4f37d5f617482b9612052d
Rachel Severson <rachel.severson@colostate.edu>
2016-09-14 18:27:25 -0600

    my old friend NSE

--- data-raw/clean_county_outlines.R
+++ data-raw/clean_county_outlines.R
@@ -8,10 +8,21 @@ library(raster)
 
 mapdata <- shapefile("~/Desktop/cb_2015_us_county_20m/cb_2015_us_county_20m.shp")
 
-mapdata@data <- mutate(mapdata@data, STATEFP = as.character(STATEFP))
-mapdata@data <- mutate(mapdata@data, COUNTYFP = as.character(COUNTYFP))
+mapdata@data <- dplyr::mutate_(mapdata@data, STATEFP = ~ as.character(STATEFP))
+mapdata@data <- dplyr::mutate_(mapdata@data, COUNTYFP = ~ as.character(COUNTYFP))
 mapdata@data$fips <- paste0(mapdata@data$STATEFP, mapdata@data$COUNTYFP)
 
-county_outlines <- mapdata
+
+
+miamidade <- mapdata[mapdata$fips == "12086",]
+
+plot(miamidade)
+
+miami_poly <- miamidade@polygons[[1]]@Polygons[[1]]@coords
+miami_poly <- as.data.frame(miami_poly)
+
+
+
 
 devtools::use_data(county_outlines, overwrite = TRUE)
+
