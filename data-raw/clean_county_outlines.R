6eaf2d7bcacc5df3b9ac6dd4a22c3318bdc7b919
Rachel Severson <rachel.severson@colostate.edu>
2016-09-24 17:32:09 -0600

    update clean_county_outlines - shapefile instead of dataframe

--- data-raw/clean_county_outlines.R
+++ data-raw/clean_county_outlines.R
@@ -1,6 +1,4 @@
 library(rgdal)
-library(dplyr)
-library(raster)
 
 # Download U.S. Census 2010 Cartographic Boundary Shapefiles - Counties from
 # https://www.census.gov/geo/maps-data/data/cbf/cbf_counties.html
@@ -8,41 +6,12 @@ library(raster)
 
 # setwd("~/Desktop/cb_2015_us_county_500k")
 
-mapdata <- readOGR("cb_2015_us_county_500k.shp", layer = "cb_2015_us_county_500k")
+shp <- readOGR(dsn = ".", layer = "cb_2015_us_county_500k")
 
-mapdata@data <- dplyr::mutate_(mapdata@data, STATEFP = ~ as.character(STATEFP))
-mapdata@data <- dplyr::mutate_(mapdata@data, COUNTYFP = ~ as.character(COUNTYFP))
-mapdata@data$fips <- paste0(mapdata@data$STATEFP, mapdata@data$COUNTYFP)
+shp@data <- dplyr::mutate_(shp@data, STATEFP = ~ as.character(STATEFP))
+shp@data <- dplyr::mutate_(shp@data, COUNTYFP = ~ as.character(COUNTYFP))
+shp@data$fips <- paste0(shp@data$STATEFP, shp@data$COUNTYFP)
 
-extractCoords <- function(sp.df){
-  results <- list()
-  for(i in 1:length(sp.df@polygons[[1]]@Polygons))
-  {
-    results[[i]] <- sp.df@polygons[[1]]@Polygons[[i]]@coords
-  }
-  results <- Reduce(rbind, results)
-  results <- as.data.frame(results)
-  results
-}
-
-fips <- mapdata@data$fips
-
-fp <- fips
-
-list <- vector("list", length(fp))
-
-for (i in 1:length(fp)){
-  coords <- extractCoords(mapdata[mapdata$fips == fp[i],])
-  coords$FIPS <- fp[i]
-
-  while(i < length(fp)){
-    list[[i]] <- coords
-    i <- i + 1
-  }
-}
-
-county_outlines <- do.call(rbind, list)
-
-colnames(county_outlines) <- c("lon", "lat", "fips")
+county_outlines <- shp
 
 devtools::use_data(county_outlines, overwrite = TRUE)
