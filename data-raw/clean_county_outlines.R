21ddae4be9c9e6209984608a8aa24aefd41b51f4
Rachel Severson <rachel.severson@colostate.edu>
2016-10-17 15:12:51 -0600

    2010 not 2015 shape files

--- data-raw/clean_county_outlines.R
+++ data-raw/clean_county_outlines.R
@@ -2,15 +2,15 @@ library(rgdal)
 
 # Download U.S. Census 2010 Cartographic Boundary Shapefiles - Counties from
 # https://www.census.gov/geo/maps-data/data/cbf/cbf_counties.html
-# download cb_2015_us_county_500k.zip
+# download gz_2010_us_050_00_500k.zip
 
-# setwd("~/Desktop/cb_2015_us_county_500k")
+# setwd("~/Desktop/gz_2010_us_050_00_500k")
 
-shp <- readOGR(dsn = ".", layer = "cb_2015_us_county_500k")
+shp <- rgdal::readOGR(dsn = ".", layer = "gz_2010_us_050_00_500k")
 
-shp@data <- dplyr::mutate_(shp@data, STATEFP = ~ as.character(STATEFP))
-shp@data <- dplyr::mutate_(shp@data, COUNTYFP = ~ as.character(COUNTYFP))
-shp@data$fips <- paste0(shp@data$STATEFP, shp@data$COUNTYFP)
+shp@data <- dplyr::mutate_(shp@data, STATE = ~ as.character(STATE))
+shp@data <- dplyr::mutate_(shp@data, COUNTY = ~ as.character(COUNTY))
+shp@data$fips <- paste0(shp@data$STATE, shp@data$COUNTY)
 
 county_outlines <- shp
 
